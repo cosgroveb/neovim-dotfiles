7778fcbc2d69c85448fc2c886b9753e90f4c899c
Hector Rios <that.hector@gmail.com>
2024-02-09 13:05:14 -0600

    feat: organize lua modules + colorscheme (#1)

---
+++ lua/options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.textwidth = 100
+vim.opt.signcolumn = "number"
+vim.opt.completeopt = "menuone,noselect"
