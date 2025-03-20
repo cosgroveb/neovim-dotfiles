0de577debc01b805e81da0a56b62b831827b95ef
Hector Rios <that.hector@gmail.com>
2025-03-20 13:49:27 -0500

    fix(options): Remove signcolumn (conflicts with gitsigns.nvim) (#21)

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.textwidth = 100
-vim.opt.signcolumn = "number"
 vim.opt.completeopt = "menuone,noselect"
 vim.opt.autoindent = true
 vim.opt.smartindent = true
