809de983a0c0efc4715ffcc876922f04774b4a46
Nick deLannoy <nick.delannoy@gmail.com>
2025-04-16 14:50:39 +0000

    add delete buffer keymap to buffer explorer

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -52,13 +52,52 @@ return {
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
 			},
+			"echasnovski/mini.bufremove",
+
 		},
 		keys = {
 			{ "<Leader><Leader>", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
 			{ "<Leader>ff", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
 			{ "<C-p>", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
 			{ "<Leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Find (grep)" },
-			{ "<Leader>be", "<cmd>Telescope buffers<CR>", desc = "Buffer Explorer" },
+			{
+				"<Leader>be",
+				function()
+					require("telescope.builtin").buffers({
+						attach_mappings = function(prompt_bufnr, map)
+							local action_state = require("telescope.actions.state")
+							local bd = require("mini.bufremove").delete
+							local delete_buffer = function()
+								local current_picker = action_state.get_current_picker(prompt_bufnr)
+								current_picker:delete_selection(function(selection)
+									local bufnr = selection.bufnr
+									local force = vim.api.nvim_buf_get_option(bufnr, "buftype") == "terminal"
+									if force then
+										return pcall(vim.api.nvim_buf_delete, bufnr, { force = force })
+									elseif vim.fn.getbufvar(bufnr, "&modified") == 1 then
+										local choice =
+										vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname(bufnr)), "&Yes\n&No\n&Cancel")
+										if choice == 1 then -- Yes
+											vim.api.nvim_buf_call(bufnr, function()
+												vim.cmd("write")
+											end)
+											return bd(bufnr)
+										elseif choice == 2 then -- No
+											return bd(bufnr, true)
+										end
+									else
+										return bd(bufnr)
+									end
+								end)
+							end
+							map("n", "d", delete_buffer)
+							map("i", "<c-d>", delete_buffer)
+							return true
+						end,
+					})
+				end,
+				desc = "Buffer Explorer"
+			}
 			{ "<Leader>gw", "<cmd>Telescope grep_string<CR>", desc = "Grep word in cursor" },
 			{ "<Leader>fh", "<cmd>Telescope man_pages<CR>", desc = "Find help pages" },
 			{ "<Leader>fm", "<cmd>Telescope keymaps<CR>", desc = "Find keymappings" },
