0a9c8e703ae375a4a9cb71ad09a1f78ee1aa4040
Nick deLannoy <nick.delannoy@gmail.com>
2025-04-17 15:20:12 +0000

    added config for snacks git tools

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -22,6 +22,21 @@ return {
 					]],
 				}
 			},
+			lazygit = {
+				config = {
+					os = {
+						edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+						editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+						editAtLineAndWait = 'nvim +{{line}} {{filename}}',
+						openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+					}
+				}
+			}
+
 		},
+		keys = {
+			{ "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+			{ "<leader>gb", function() Snacks.git.blame_line() end, desc = "[G]it [B]lame"}
+		}
 	},
 }
