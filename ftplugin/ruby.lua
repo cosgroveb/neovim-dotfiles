05ecc562375eadbb177f8d8ef93add6231dcb21a
Brian Cosgrove <cosgroveb@gmail.com>
2025-04-30 22:35:12 -0500

    feat(legacy): port \rs syntax check for ruby files (#85)

--- ftplugin/ruby.lua
+++ ftplugin/ruby.lua
@@ -1,2 +1,3 @@
 -- mnemonic: ruby-debug
 vim.keymap.set("", "<LocalLeader>rd", "Orequire 'pry'; binding.pry<ESC>", { desc = "[r]uby [d]ebug: binding.pry line under cursor" })
+vim.keymap.set("n", "<silent> <LocalLeader>rs", ":!ruby -c %<CR>", { desc = "[r]uby [s]yntax: check the syntax of the current file" })
