df6d0711f55e1525a22d4c9da929a67496603a47
Brian Cosgrove <cosgroveb@gmail.com>
2025-06-17 15:49:00 -0500

    feat(legacy): vim-plug command aliases (#172)
    Redirect vim-plug commands to Lazy equivalents and display a warning.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,20 @@ autocmd("VimEnter", {
         end, 1000)
     end,
 })
+
+-- Redirect vim-plug commands to Lazy equivalents
+for plug_cmd, lazy_cmd in pairs({
+    Plug = "",
+    PlugInstall = "install",
+    PlugUpdate = "update", 
+    PlugUpgrade = "update",
+    PlugDiff = "log",
+    PlugClean = "clean",
+    PlugStatus = "",
+    PlugSnapshot = "profile"
+}) do
+    vim.api.nvim_create_user_command(plug_cmd, function()
+        vim.notify("Lazy has replaced Plug. Executing :Lazy " .. lazy_cmd, vim.log.levels.WARN)
+        vim.defer_fn(function() vim.cmd('Lazy ' .. lazy_cmd) end, 2000)
+    end, { desc = "Redirect " .. plug_cmd .. " to Lazy " .. lazy_cmd })
+end
